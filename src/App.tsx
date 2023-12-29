bd75c07f2869e963428b8449f295138f5d07634f
Fernando <comododk@gmail.com>
2023-12-29 02:03:36 -0300

    feat: default theme and global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyles } from './styles/global'
+import { Header } from './components/Header'
 
 export function App() {
-
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
-      <h1>
-        Hello World
-      </h1>
+      <Header />
     </ThemeProvider>
   )
 }

9dfd799ab2f588d26d9e35d2f9072dc917923f08
Fernando <comododk@gmail.com>
2023-12-31 19:07:28 -0300

    refactor: summary component and add usesummary hook

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -3,31 +3,13 @@ import {
   ArrowCircleUp,
   CurrencyDollar,
 } from '@phosphor-icons/react'
-import { useContext } from 'react'
-import { TransactionContext } from '@/contexts/TransactionContext'
+import { useSummary } from '@/hooks/useSummary'
+import { priceFormatter } from '@/utils/formatter'
 
 import * as S from './styles'
 
 export const Summary = () => {
-  const { transactions } = useContext(TransactionContext)
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price
-        acc.total += transaction.price
-      } else {
-        acc.outcome += transaction.price
-        acc.total -= transaction.price
-      }
-
-      return acc
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  )
+  const summary = useSummary()
 
   return (
     <S.SummaryContainer>
@@ -37,7 +19,7 @@ export const Summary = () => {
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
-        <strong>{summary.income}</strong>
+        <strong>{priceFormatter.format(summary.income)}</strong>
       </S.SummaryCard>
       <S.SummaryCard>
         <header>
@@ -45,7 +27,7 @@ export const Summary = () => {
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
-        <strong>{summary.outcome}</strong>
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
       </S.SummaryCard>
       <S.SummaryCard variant="green">
         <header>
@@ -53,7 +35,7 @@ export const Summary = () => {
           <CurrencyDollar size={32} color="#fff" />
         </header>
 
-        <strong>{summary.total}</strong>
+        <strong>{priceFormatter.format(summary.total)}</strong>
       </S.SummaryCard>
     </S.SummaryContainer>
   )
